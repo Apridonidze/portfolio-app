6e2f5b88a243397ccf334c44e75a48c427f28521
Giorgi Aphridonidze <giorgiapridonidze08@gmail.com>
2026-04-28 03:26:17 +0400

    style : impelemteted layout for Main.jsx page with responsive view handling

--- src/container/Main.jsx
+++ src/container/Main.jsx
@@ -4,10 +4,10 @@ import '../style/main.css'
 
 export default function Main(){
     return(
-        <div className="main-container">
+        <div className="main-container container-fluid row row-cols-sm-2 row-cols-1">
             <div className="main-start"><Sidebar /></div>
-            <div className="main-end">
-
+            <div className="main-end mx-auto border">
+                asdadsadsad
             </div>
         </div>
     );
