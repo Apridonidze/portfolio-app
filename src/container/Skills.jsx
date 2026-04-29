c29ca35ded983230a370363afad66f7d5a3e2ae4
Giorgi Aphridonidze <giorgiapridonidze08@gmail.com>
2026-04-29 04:11:14 +0400

    feat :added content in Skills.jsx component

--- src/container/Skills.jsx
+++ src/container/Skills.jsx
@@ -1,4 +1,5 @@
 import { useRef, useEffect } from "react"
+import Skill from "../components/Skill"
 
 export default function Skills({ setActiveSection }){
 
@@ -13,6 +14,53 @@ export default function Skills({ setActiveSection }){
 
     },[setActiveSection])
 
+    const skills = [
+        {
+            title: 'Frontend',
+            skills: [
+            'React',
+            'JavaScript',
+            'HTML',
+            'CSS',
+            'Bootstrap',
+            'State Management',
+            'recharts.js'
+            ]
+        },
+        {
+            title: 'Backend',
+            skills: [
+            'Node.js',
+            'ExpressJS',
+            'REST API',
+            'WebSockets',
+            'Webhooks',
+            'JWT',
+            'Zod',
+            'Rate Limiting'
+            ]
+        },
+        {
+            title: 'Database & Integrations',
+            skills: [
+            'MySQL',
+            'Stripe',
+            'SendGrid',
+            'Third-Party Integration'
+            ]
+        },
+        {
+            title: 'Tools & Deployment',
+            skills: [
+            'Git',
+            'GitHub',
+            'Docker',
+            'Vercel',
+            'Railway',
+            ]
+        }
+    ];
+
     return(
         <section id="Skills" ref={sectionRef}>
 
@@ -20,13 +68,7 @@ export default function Skills({ setActiveSection }){
             <h1 className="section-title ">Skills & Technologies</h1>
 
             <div className="section-body">
-                <h6 className="my-3">I’m a <b>full-stack developer</b> focused on building real-world web applications that solve practical problems and scale over time. I work across the entire stack using <b className="lightHighlight">React, Node.js, Express, and MySQL,</b> delivering production-ready products.</h6>
-
-                <h6 className="my-3">My experience comes from <b>freelance projects</b> where I handled everything from planning to deployment. This helped me build strong communication skills and the ability to deliver features under real constraints with a product-focused mindset.</h6>
-
-                <h6 className="my-3">I focus on clean architecture & scalability, performance, and usability, building systems that are maintainable and scalable. I can take an idea or design and turn it into a complete application or improve existing systems.</h6>
-
-                <h6 className="my-3">Currently, <b className="lightHighlight">I’m looking for opportunities</b> to contribute to real products, collaborate with teams, and continue growing as a developer through meaningful work.</h6>
+                {skills.map(skill => <Skill title={skill.title} skills={skill.skills}/>)}
             </div>
 
         </section>
