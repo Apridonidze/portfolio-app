5c5ebe7ba8f740fded50ca29bb7aae9c812c5719
Giorgi Aphridonidze <giorgiapridonidze08@gmail.com>
2026-05-01 03:40:34 +0400

    fix : fixed Project.jsx urls to github

--- src/components/Project.jsx
+++ src/components/Project.jsx
@@ -8,7 +8,7 @@ export default function Project({ project, projectId }){
                 <h4>{project.title}</h4>
                 <h6>{project.desc}</h6>
                 <div className="row gap-2 my-2 px-2">{project.usedTech.map(tech => <span className="usedTechnology col-auto">{tech}</span> )}</div>
-                <button onClick={() => window.open(project.githubUrl)} className="githubBtn btn mt-2 w-100"><i class="fa-brands fa-github"></i> View on GitHub</button>
+                <button onClick={() => window.open(project.githubUrl, "_blank", "noopener,noreferrer")} className="githubBtn btn mt-2 w-100"><i className="fa-brands fa-github"></i> View on GitHub</button>
             </div>
         </div>
     );
